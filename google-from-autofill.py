01abfd44fa035283d7f97945f478c07a5837204b
Tanabordee Pateepbutsagon <102315593+Tanabordeee@users.noreply.github.com>
2024-05-22 13:15:18 +0700

    update detect human error

--- google-from-autofill.py
+++ google-from-autofill.py
@@ -29,8 +29,6 @@ link = input('Link: ')
 shuffle_option = input("Do you want to shuffle (y/n)? : ")
 
 if shuffle_option == 'y':
-    element_container = int(input("How many container :"))
-    shuffled_elements = [[] for _ in range(element_container)]
     print("PLEASE ADD ALL ELEMENTS")
     print("IF FINISH PLEASE INPUT (end)")
     while True:
@@ -41,55 +39,97 @@ if shuffle_option == 'y':
             element_xpath = input('Xpath of textbox: ')
             answer = input('Your answer: ')
             textboxes[element_xpath] = answer
-        else:
+        elif textbox_option == "no":
+            while True:
+                    try:
+                        element_container = int(input("How many container :"))
+                        break
+                    except ValueError:
+                        print("Invalid input. Please enter a valid integer.")
+            shuffled_elements = [[] for _ in range(element_container)]
             for m in range(element_container):
                 shuffled_elements[m] = []
-                container_size = int(input("How many items in container: "))
+                while True:
+                    try:
+                        container_size = int(input("How many items in container: "))
+                        break
+                    except ValueError:
+                        print("Invalid input. Please enter a valid integer.")
                 for n in range(container_size):
-                    element = input('Button or radio element: ')
+                    print("number" , n+1 , end=" | ")
+                    element = input('Button or radio element : ')
                     shuffled_elements[m].append(element)
-
-    loop_count = input('Input number of loops: ')
-    loop_count = int(loop_count)
+        else:
+            continue
+    while True:
+        try:
+            loop_count = input('Input number of loops: ')
+            loop_count = int(loop_count)
+            break
+        except Exception as e:
+            continue
 
     for i in range(loop_count):
+        print("NUMBER : ", i + 1)
         web = webdriver.Chrome()
         web.get(link)
         time.sleep(2)
         selected_items = [random.choice(sublist) for sublist in shuffled_elements]
 
         for xpath, answer in textboxes.items():
-            entry_textbox = web.find_element('xpath', xpath)
-            entry_textbox.send_keys(answer)
+            try:
+                entry_textbox = web.find_element('xpath', xpath)
+                entry_textbox.send_keys(answer)
+            except Exception as e:
+                continue
 
         for item_xpath in selected_items:
-            element = web.find_element('xpath', item_xpath)
-            element.click()
+            try:
+                element = web.find_element('xpath', item_xpath)
+                element.click()
+            except Exception as e:
+                continue
         time.sleep(2)
 
 else:
-    element_count = input("How many elements in your form: ")
-    element_count = int(element_count)
+    while True:
+        try:
+            element_count = input("How many elements in your form: ")
+            element_count = int(element_count)
+            break
+        except ValueError:
+            continue
     for k in range(element_count):
         textbox_option = input("Do you have a textbox? (yes/no): ")
-        button_option = input("Do you have a button? (yes/no): ")
         if textbox_option == 'yes':
             textbox_xpath = input('Xpath of textbox: ')
             answer = input('Your answer: ')
             textboxes[textbox_xpath] = answer
-        else:
+        else :
             element_xpath = input('Button or radio element: ')
             radio_button_elements.append(element_xpath)
-    loop_count = input('Input number of loops: ')
-    loop_count = int(loop_count)
+    while True:
+        try:   
+            loop_count = input('Input number of loops: ')
+            loop_count = int(loop_count)
+            break
+        except Exception as e:
+            continue
 
     for i in range(loop_count):
+        print("NUMBER : ", i + 1)
         web = webdriver.Chrome()
         web.get(link)
         time.sleep(2)
         for xpath, answer in textboxes.items():
-            entry_textbox = web.find_element('xpath', xpath)
-            entry_textbox.send_keys(answer)
+            try:
+                entry_textbox = web.find_element('xpath', xpath)
+                entry_textbox.send_keys(answer)
+            except Exception as e:
+                continue
         for xpath in radio_button_elements:
-            element = web.find_element('xpath', xpath)
-            element.click()
+            try:
+                element = web.find_element('xpath', xpath)
+                element.click()
+            except Exception as e:
+                continue
